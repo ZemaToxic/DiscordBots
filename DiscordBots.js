f520f793fe3256423319a90951ecb1404f8ada1b
ZEMATOXIC-PC\ZemaToxic <mseymour.home@gmail.com>
2019-01-21 19:49:42 +1300

    Update .gitignore

--- DiscordBots.js
+++ DiscordBots.js
@@ -1,11 +1,3 @@
-// Console Colour Code
-const RESET_COLOR = '\x1b[0m';
-const COLOR_WHITE = '\x1b[1m\x1b[0m';
-const COLOR_RED = '\x1b[1m\x1b[31m';
-const COLOR_GREEN = '\x1b[1m\x1b[32m';
-const COLOR_CYAN = '\x1b[1m\x1b[36m';
-const COLOR_YELLOW = '\x1b[1m\x1b[33m';
-
 const { spawn } = require('child_process');
 const { lstatSync, readdirSync } = require('fs');
 const { join } = require('path');
@@ -14,39 +6,41 @@ var fs = require('fs');
 
 const discordBots = {};
 
-// Directory Naviagtion
+// Directory Navigation
 const isDir = source => lstatSync(source).isDirectory();
 const getDirs = source => readdirSync(source).map(name => join(source, name)).filter(isDir);
 
 var directories = getDirs('./');
+console.log(process.cwd());
 
 directories.forEach(function( v ){
+	
 	// Search through each folder looking for app.js
-	console.log(COLOR_YELLOW, timeStamp(), 'Searching for ./' + v + '/app.js', RESET_COLOR);
+	console.log(timeStamp(), 'Searching for ./' + v + '/app.js');
 
 	if ( fs.existsSync('./'+v+'/app.js') ) {
 
 		// Make a new [object?] for each bot client
 		discordBots[v] = {};
 		discordBots[v].start = function () {
-			
-			// Start up a new bot client 
-			console.log(COLOR_GREEN, timeStamp(), 'Starting Bot ' + v, RESET_COLOR);
-			
-			const bot = discordBots[v].process = spawn('node', [v + '/app.js']);
 
+			// Start up a new bot client 
+			console.log(timeStamp(), 'Starting Bot ' + v);
+			
+			const bot = discordBots[v].process = spawn('node', [ v + '/app.js'], {shell: true});
+			
 			// Console logging 
 			bot.stdout.on('data', (d) => {
-				console.log(COLOR_WHITE, timeStamp(), 'DATA FROM ' + v + ': ' + d, RESET_COLOR);
+				console.log(timeStamp(), 'DATA FROM ' + v + ': ' + d);
 			});
 			// If the child has an error printed out            
 			bot.stderr.on('data', (data) => {
-				console.log(COLOR_CYAN, timeStamp(), 'DATA FROM ' + v + ': stderr: ' + data, RESET_COLOR);
+				console.log( timeStamp(), 'DATA FROM ' + v + ': stderr: ' + data);
 			});
 
 			// What to do when each bot child closes
 			bot.on('close', (code) => {
-				console.log(COLOR_RED, timeStamp(), 'DATA FROM ' + v + ': Process exited with code ' + code, RESET_COLOR);
+				console.log( timeStamp(), 'DATA FROM ' + v + ': Process exited with code ' + code);
 				discordBots[v].start();
 			});
 		};
@@ -64,20 +58,3 @@ function timeStamp() {
 	var minutes = now.getMinutes();
 	return '[' + date + '/' + month + '/' + year + ' - ' + hours + ':' + minutes + ']';
 }
-
-// Process listeners 
-process.on('exit', (code) =>
-{
-	console.log('Process exited with code: ' + code);
-});
-
-process.on('unhandledRejection', err =>
-{
-	console.error('Unhandled Rejection: \n', err);
-});
-
-process.on('error', err =>
-{
-	console.error('Error happened: \n ', err);
-});
-
