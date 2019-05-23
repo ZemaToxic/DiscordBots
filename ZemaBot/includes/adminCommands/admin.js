3a13fb0f04af4347b53adc963b9acabdc7e72219
Crystal <belladonnacrystal@gmail.com>
2019-05-23 15:40:30 +1200

    changes

---
+++ ZemaBot/includes/adminCommands/admin.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "admin",
+    description: "Admin override to set settings.",
+    execute(client, guildConf, message, args) {
+
+    // Let's get our key and value from the arguments. 
+    // This is array destructuring, by the way. 
+    const [prop, ...value] = args;
+    // Example: 
+    // prop: "prefix"
+    // value: ["+"]
+    // (yes it's an array, we join it further down!)
+    
+    // We can check that the key exists to avoid having multiple useless, 
+    // unused keys in the config:
+    if(!client.settings.has(message.guild.id, prop)) {
+      return message.reply("This key is not in the configuration.");
+    }
+    
+    // Now we can finally change the value. Here we only have strings for values 
+    // so we won't bother trying to make sure it's the right type and such. 
+    client.settings.set(message.guild.id, value.join(" "), prop);
+    
+    // We can confirm everything's done to the client.
+    message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+  }
+};
