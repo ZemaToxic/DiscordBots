ff104d8ac227fb9ddfed06e9a13a9058d8355958
ZEMATOXIC-PC\ZemaToxic <mseymour.home@gmail.com>
2019-05-24 22:36:45 +1200

    i dont know at this point...

--- ZemaBot/app.js
+++ ZemaBot/app.js
@@ -34,11 +34,11 @@ const corsOptions = {
   }
 }
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// });
 
 app.use(cors())
 app.set('json spaces', 2);
