7eae3d82b738d3d698a1b24d7212d7f0752a4449
ZEMATOXIC-PC\ZemaToxic <mseymour.home@gmail.com>
2019-05-27 10:17:34 +1200

    fix imports

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userService = require('./user.service');
+const userService = require('./user.service.js');
 
 // routes
 router.post('/authenticate', authenticate);
