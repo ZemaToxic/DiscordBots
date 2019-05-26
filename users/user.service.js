62d794edef83705878a5f881b95b42e9dc3046d5
ZEMATOXIC-PC\ZemaToxic <mseymour.home@gmail.com>
2019-05-27 10:21:18 +1200

    Removed reports, fixed require paths

--- users/user.service.js
+++ users/user.service.js
@@ -1,7 +1,7 @@
 const config = require('config.json');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const db = require('_helpers/db.js');
+const db = require('./_helpers/db.js');
 const User = db.User;
 
 module.exports = {
