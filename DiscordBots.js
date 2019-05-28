beed6da996f4018fbbdef62bba0c3f8d6075f6ab
ZEMATOXIC-PC\ZemaToxic <mseymour.home@gmail.com>
2019-05-28 19:35:39 +1200

    I think i now have this sorted

--- DiscordBots.js
+++ DiscordBots.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
  });
--- users/users.controller.js
+++ users/users.controller.js
@@ -11,6 +11,15 @@ router.get('/:id', getById);
 router.put('/:id', update);
 router.delete('/:id', _delete);
 
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+ });
+
+
+
 module.exports = router;
 
 function authenticate(req, res, next) {
